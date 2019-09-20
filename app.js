754f499048c8be0bff78eb17da32ed7dd18ac879
Matt Hawes <matt@hawes.xyz>
2019-09-20 00:22:05 -0400

    remove commented line

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const port = 3000;
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
 
 app.set('view engine', 'ejs')
-// app.set('views', './src') // Change default location from ./views to ./src
-app.use('/public', express.static('./public/'));
+app.use('/public', express.static('./public/'))
 app.use('/lib', express.static('./bin/lib/'))
 
 app.get('/', (req, res) => {
