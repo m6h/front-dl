60c6478565dc89cab3d7055b3cfe2bc6bfd355a1
Matt Hawes <matt@hawes.xyz>
2019-09-20 00:25:51 -0400

    cleanup

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+const express = require('express')
+const cmd = require('node-cmd')
+const app = express()
+const port = 3000
 
+app.listen(port, () => console.log(`listening on http://localhost:${port}`))
 app.set('view engine', 'ejs')
 app.use('/public', express.static('./public/'))
 app.use('/lib', express.static('./bin/lib/'))
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
-});
+    res.render('./index.ejs')
+})
