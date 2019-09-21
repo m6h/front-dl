34a5a8650423065c4c5ae9ed6e77b97b39356a24
Matt Hawes <matt@hawes.xyz>
2019-09-21 17:17:32 -0400

    update navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,26 @@
 import m from '../lib/mithril'
 
+function toggleNavBurger() {
+    document.querySelector('.navbar-burger').classList.toggle('is-active')
+    document.querySelector('.navbar-menu').classList.toggle('is-active')
+}
+
 export var navbar = {
-    view: () => [
-        m('h1', 'youtube-dl'),
-        m('navbar', [
-            m('button', 'audio'),
-            m('button', 'video'),
+    view: () => m('nav', {class: 'navbar'}, [
+        m('div', {class: 'navbar-brand'} , [
+            m('a', {class: 'navbar-item', href:'#!/'}, [
+                m('a', {class: 'title'}, 'youtube-dl')
+            ]),
+            m('a', {role: 'button', onclick: toggleNavBurger, class: 'navbar-burger'}, [
+                m('span'), m('span'), m('span')
+            ])
+        ]),
+        m('div', {class: 'navbar-menu'}, [
+            m('div', {class: 'navbar-start'}, [
+                m('a', {class: 'navbar-item', href: '#!/audio'}, 'audio'),
+                m('a', {class: 'navbar-item', href: '#!/video'}, 'video'),
+            ]),
+            m('div', {class: 'navbar-end'}, '')
         ])
-    ]
+    ])
 }
