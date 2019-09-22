f76888136c4267125e6159a6153fdf87a598b4f0
Matt Hawes <matt@hawes.xyz>
2019-09-21 20:40:23 -0400

    add navbar separator

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,21 +6,24 @@ function toggleNavBurger() {
 }
 
 export var navbar = {
-    view: () => m('nav', {class: 'navbar'}, [
-        m('div', {class: 'navbar-brand'} , [
-            m('a', {class: 'navbar-item', href:'#!/'}, [
-                m('a', {class: 'title'}, 'youtube-dl')
+    view: () => [
+        m('nav', {class: 'navbar'}, [
+            m('div', {class: 'navbar-brand'} , [
+                m('a', {class: 'navbar-item', href:'#!/'}, [
+                    m('a', {class: 'title'}, 'youtube-dl')
+                ]),
+                m('a', {role: 'button', onclick: toggleNavBurger, class: 'navbar-burger'}, [
+                    m('span'), m('span'), m('span')
+                ])
             ]),
-            m('a', {role: 'button', onclick: toggleNavBurger, class: 'navbar-burger'}, [
-                m('span'), m('span'), m('span')
+            m('div', {class: 'navbar-menu'}, [
+                m('div', {class: 'navbar-start'}, [
+                    m('a', {class: 'navbar-item', href: '#!/audio'}, 'audio'),
+                    m('a', {class: 'navbar-item', href: '#!/video'}, 'video'),
+                ]),
+                m('div', {class: 'navbar-end'}, '')
             ])
         ]),
-        m('div', {class: 'navbar-menu'}, [
-            m('div', {class: 'navbar-start'}, [
-                m('a', {class: 'navbar-item', href: '#!/audio'}, 'audio'),
-                m('a', {class: 'navbar-item', href: '#!/video'}, 'video'),
-            ]),
-            m('div', {class: 'navbar-end'}, '')
-        ])
-    ])
+        m('hr', {style: 'margin-top: 0'})
+    ]
 }
