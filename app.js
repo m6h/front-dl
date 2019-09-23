457e36a61395a25b9b7194e0a47d84d4228acc49
Matt Hawes <matt@hawes.xyz>
2019-09-23 04:18:57 -0400

    experiment with spawn vs exec

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const exec = require('child_process').exec
+const {exec} = require('child_process')
 const app = express()
 const port = 3000
 
@@ -24,19 +24,28 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/browse', (req, res) => {
-    exec(`find ./node_modules/${req.query.path} -maxdepth 1 -mindepth 1 -type d -printf '%f/'`, (error, stdout, stderr) => {
+    exec(`find "./node_modules/${req.query.path}" -maxdepth 1 -mindepth 1 -type d -printf '%f/'`, (error, stdout, stderr) => {
         console.log(stdout)
+        console.log(stderr)
         res.json(stdout)
     })
-    // exec(`ls -F ./node_modules/${req.query.path} | grep "/$"`, (error, stdout, stderr) => {
-    //     res.send(stdout)
+    // var find = spawn('find', [`./node_modules/${req.query.path}`, '-maxdepth', '1', '-mindepth', '1', '-type', 'd', '-printf', '%f/'])
+    // console.log(find.spawnargs)
+    // find.stdout.once('data', data => {
+    //     console.log(data.toString())
+    //     res.json(data.toString())
+    // })
+    // if folder is empty will return exit code 0
+    // find.on('exit', data => {
+    //     console.log('')
+    //     res.json('')
     // })
 })
 
 app.get('/api/ydl/:outputString/:url', (req, res) => {
     var command = `echo ${req.params.outputString} ${req.params.url}`
 
-    var cmd = exec(command, (error, stdout, stderr) => {
+    var cmd = spawn(command, (error, stdout, stderr) => {
         // stdout ? res.send('done') : res.send(error)
         res.json(stdout)
     })
