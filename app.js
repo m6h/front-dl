e6bc17d511d1f283fb748a9cddc74cd116e4d016
Matt Hawes <matt@hawes.xyz>
2020-02-17 15:38:40 -0500

    mongoose connection parameters

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const app = express()
 const port = 3000
 const server = app.listen(port, () => console.log(`listening on http://localhost:${port}`))
 const io = require('socket.io').listen(server)
+const mongoose = require('mongoose')
 
+// Satisfy Mongoose deprecation warnings
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useUnifiedTopology', true)
+
+// MongoDB connection using Mongoose. Log any initial connection errors.
+mongoose.connect('mongodb://mongodb/front-dl').catch(e => console.error(e))
+
+// Listen for connection errors after initial connection
+mongoose.connection.on('error', e => console.error(e))
 
 // Export Socket.io
 exports.io = io
