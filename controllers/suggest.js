971d7fa95228f1c7a3157144e84234820aae8321
Matt Hawes <matt@hawes.xyz>
2020-02-17 19:19:36 -0500

    genres suggestions controller

---
+++ controllers/suggest.js
@@ -0,0 +1,31 @@
+const { Genre } = require('../models/suggest')
+
+// Get all genres
+exports.getGenre = async (req, res) => {
+    const genres = await Genre.find()
+    res.json(genres)
+}
+
+// Add or update genres. New genres are added, existing genres have their weight incremented by 1.
+exports.updateGenre = async (req, res) => {
+    const genre = await Genre.findOne({name: req.params.name})
+
+    if (genre) {
+        // Already exists. Increment weight.
+        genre.weight++
+        await genre.save().catch(e => console.error(e))
+        res.json(genre)
+    } else {
+        // Genre was not found. Add the new genre.
+        const document = new Genre({name: req.params.name})
+
+        await document.save().catch(e => console.error(e))
+        res.json(document)
+    }
+}
+
+// Delete a genre
+exports.deleteGenre = async (req, res) => {
+    const document = await Genre.remove({name: req.params.name})
+    res.json(document)
+}
