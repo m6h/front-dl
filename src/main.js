b0fa6880a684e7ee7e8697515140a4dfbee43441
Matt Hawes <matt@hawes.xyz>
2020-04-11 20:52:05 -0400

    Replace app's settings/preferences class with object

--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
 import m from 'mithril'
-import io from 'socket.io-client'
 import navbar from './components/navbar'
 import download from './views/download'
 import playlist from './views/playlist'
 import settings from './views/settings'
 import directory from './components/directoryBrowser'
 
-// App's settings/preferences. Global app state. Singleton class.
-class Settings {
-    set(prefs) {
-        this.prefs = prefs
-    }
+// App's global settings/preferences state.
+export const app = {
+    prefs: {}
 }
-
-// Create new instance of class. Export same instance for all imports.
-var app = new Settings
-export { app }
-
-// Initialize socket for the app
-export const socket = io()
 
 // The root mithril component. Used to add static page elements.
 const root = {
@@ -34,7 +24,7 @@ m.request({
     responseType: 'json',
     url: '/api/settings'
 }).then(response => {
-    app.set(response)
+    app.prefs = response
 
     // Initialize directory's folder list on first run
     directory.get()
